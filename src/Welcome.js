14ce377a2a815cf22f606d72072dea6cd501d375
Jimmy Zhang <imyjimmy@gmail.com>
2024-05-11 21:42:11 -0500

    up arrow icon in welcome msg

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Welcome.css"
+import { UpArrowIcon } from './UpArrowIcon';
 
 const Welcome = ({}) => {
 
   return (
     <div className="welcome-message">
-      <h2>Input a bitcoin transaction here</h2>
+      <h2>Input a bitcoin transaction here <UpArrowIcon className="up-arrow" /></h2>
+      
       <p>Here are some bitcoin transactions in the wild:</p>
       <ul>
         <li>Go to <a href="https://mempool.space/api/tx/e226ef849f03a08a9d8e15f949a1d47182fb5c46acf7158b78e6c011f6aba244/hex">this</a> site, select all &gt; copy. Then paste it up above. The way <a href="mempool.space">mempool.space</a> likes to visualize this transaction is <a href="https://mempool.space/tx/e226ef849f03a08a9d8e15f949a1d47182fb5c46acf7158b78e6c011f6aba244">here</a></li>
