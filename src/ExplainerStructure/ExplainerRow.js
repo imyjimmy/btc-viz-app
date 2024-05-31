ff97d0f9c8a35cf0f1c35eea5975cc9a7547fbd2
Jimmy Zhang <imyjimmy@gmail.com>
2024-05-31 13:28:20 -0500

    explainer styling

--- src/ExplainerStructure/ExplainerRow.js
+++ src/ExplainerStructure/ExplainerRow.js
@@ -5,12 +5,12 @@ const ExplainerRow = ({ colorCode, hex, keyName, entry }) => {
     hex && (<div className="explainer-row">
       <div className={`explainer-hex-value ${colorCode}`}>{hex.length > 8 ? (hex.substring(0,8)+'...') : (hex)}</div>
       <div className="explainer-key"><div className="key-name">{keyName}</div></div>
-      <div className="explainer-val"><code>{entry.bytes ?? ''}</code> 
-      {entry.int ? (`${entry.bytes} => ${entry.int}`):(<></>)}</div>
-      {/* ternary on keyName == length... */}
-      {/* { Object.keys(entry).filter((key) => key !== 'hex').map((k) => {
-        return (<div className="explainer-val"><span className="interpreted-val">{entry[k]}</span></div>)
-      })} */}
+      <div className="explainer-val">{keyName === 'length' && entry.int ? (<><code>{entry.bytes}</code> <span>=&gt;</span>{entry.int} bytes long</>
+      ):(<code>{entry.bytes ?? ''}</code> )}</div>
+        {/* ternary on keyName == length... */}
+        {/* { Object.keys(entry).filter((key) => key !== 'hex').map((k) => {
+          return (<div className="explainer-val"><span className="interpreted-val">{entry[k]}</span></div>)
+        })} */}
     </div>)
   );
 }
