87a464c77c275c6061f41c4481f8e57059cf0e5b
Jimmy Zhang <imyjimmy@gmail.com>
2024-06-13 23:27:13 -0500

    animation expand-collapse accordion

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,16 +1,32 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import styles from './Accordion.module.css';
 
 const Accordion = ({ title, titleNode, children }) => {
   const [isActive, setIsActive] = useState(false);
+  const [maxHeight, setMaxHeight] = useState('0px');
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (isActive) {
+      setMaxHeight(`${contentRef.current.scrollHeight}px`);
+    } else {
+      setMaxHeight('0px');
+    }
+  }, [isActive]);
 
   return (
     <div className={styles["accordion-item"]}>
       <div className={styles["accordion-title"]} onClick={() => setIsActive(!isActive)}>
-        {title ? (<h3>{title}</h3>):(<>{titleNode}</>)}
+        {title ? (<h3>{title}</h3>) : (<>{titleNode}</>)}
         <h5>{isActive ? '-' : '+'}</h5>
       </div>
-      {isActive && <div className={styles["accordion-content"]}>{children}</div>}
+      <div
+        ref={contentRef}
+        style={{ maxHeight }}
+        className={styles["accordion-content"]}
+      >
+        {children}
+      </div>
     </div>
   );
 };
